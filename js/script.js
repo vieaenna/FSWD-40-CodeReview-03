c62066cfb415ed19b10ddfdfd8771c046d2f41d5
Anna <anna0706@gmx.at>
2018-04-28 13:01:23 +0200

    CodeReview JS, HTML, Sass

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
-// (20) Create a new function calculateInsurance(). This function does a calculation with a formula based on the selected country:
-
-//For Austria : insurance = horse_power x 100 / age   + 50;
-
-//For Germany: insurance = horse_power x 120 / age  + 100;
-
-// For Italy: insurance = horse_power x 150 / (age+2)  +  150;
-
-
-// (15) Extract the values from the input fields and use them as calculation values for calculateInsurance() function. (hint: pay attention to type conversion!)
-
-// (15) Use these calculated values to present the insurance price on the screen where "printed calculation goes here" is placed (see image example). e.g. “John, your insurance costs 300 €”
-
-// Note:  to be capable of getting bonus points, a function like  calculateInsurancePerCountry(horse_power, age, country)  that returns a value that can be tested will be needed.
+// Basic, but working solution - if else condition
 
 function calculateInsurance () { 
 	name = document.getElementById('name').value;
